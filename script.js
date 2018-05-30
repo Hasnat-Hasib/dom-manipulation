c85db5d5724a61a94f696c3a13409e5bb7f4d10f
Hasnat Hasib <hasib8nh@gmail.com>
2018-05-30 02:58:16 +0000

    Task2a

--- script.js
+++ script.js
@@ -5,6 +5,15 @@
 
 /* Task 2 using plain JavaScript */
 
+    function addText2(){
+        var task2div = document.getElementById('task2a')
+        var paragraph = document.createElement('p')
+        paragraph.innerText = "Hello World"
+        task2div.appendChild(paragraph)
+    }
 
+
+    var textBtn = document.getElementById('textBtn2a')
+    textBtn.addEventListener('click', addText2)
 
 /* Task 4 using jQuery */
